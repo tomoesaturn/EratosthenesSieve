15295c14a329b8dcef7792c3cc78dc93572a3980
tomoesaturn <42309359+tomoesaturn@users.noreply.github.com>
2020-05-11 01:09:57 +0900

    20200511

---
+++ EratosthenesSieve.java
@@ -0,0 +1,28 @@
+import java.util.function.Supplier;
+import java.util.stream.LongStream;
+
+public class EratosthenesSieve {
+    public static void main(String[] args) {
+        long N = 100L;
+        StringBuilder sb = new StringBuilder();
+
+        Supplier<LongStream> sup = () -> LongStream.rangeClosed(2, N);
+        do {
+            sup = filter(sup, sb, (long) Math.sqrt(N));
+        } while (sup != null);
+
+        //最後の","を消す
+        sb.deleteCharAt(sb.length() - 1);
+        System.out.println(sb);
+    }
+
+    static Supplier<LongStream> filter(Supplier<LongStream> stream, StringBuilder sb, long sqrt) {
+        long l = stream.get().findFirst().getAsLong();
+        if (l > sqrt) {
+            stream.get().parallel().forEach(i -> sb.append(i + ","));
+            return null;
+        }
+        sb.append(l + ",");
+        return () -> stream.get().filter(i -> i % l != 0);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # EratosthenesSieve
 Eratosthenes' Sieve for Java
+
+I use lazy evaluation of Stream API and lambdas in order to implement Eratosthenes' sieve algorithm.
